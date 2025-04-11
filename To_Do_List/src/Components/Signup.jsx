45d64a1a7830b341bc9db1868abd6b800f33cf04
Parth-Chikalkar <parthchikalkar56@gmail.com>
2025-04-12 02:03:25 +0530

    Fixed Signup

--- To_Do_List/src/Components/Signup.jsx
+++ To_Do_List/src/Components/Signup.jsx
@@ -18,14 +18,15 @@ alert(res.data.message);
 if(res.status==200){
     sessionStorage.setItem('Token',res.data.token);
 }
-
+setLoading(false);
+nav('/');
  }
  
    
   return (
     <div className='bg-zinc-900 flex items-center justify-center w-full h-screen '>
          <div className=' w-96 text-white bg-linear-to-br flex items-center p-5 flex-col from-purple-400 shadow-lg shadow-zinc-6=500 to-purple-700 rounded-lg border-1 border-amber-50 '>
-       <h1 className='text-3xl  font-semibold'>Sign In </h1>
+       <h1 className='text-3xl  font-semibold'>Sign Up </h1>
         <form action="" className='w-full mt-3 flex flex-col gap-2' onSubmit={HandleSubmit}>
             <h3 className='font-thin text-sm' >Username</h3>
             <input onChange={(e)=>setusername(e.target.value)} className='w-full bg-white/40 rounded-md p-2 h-10' type="text" required/>
